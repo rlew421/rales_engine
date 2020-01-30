37d6d4060b71b4d2ddfeac04a3ffd0224ee378a2
Rachel Lew <rlew07@gmail.com>
2020-01-30 13:34:14 -0700

    Add route to get all invoices associated with one customer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :customers do
+        get ':id/invoices', to: 'invoices#index'
+      end
       namespace :merchants do
         get ':id/items', to: 'items#index'
         get ':id/invoices', to: 'invoices#index'
