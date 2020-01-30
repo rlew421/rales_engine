ff4020f9a9d3ef5384740e1271ae99e14ee78fbd
Rachel Lew <rlew07@gmail.com>
2020-01-30 16:18:08 -0700

    Add route to return invoice items associated with one invoice

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
       namespace :invoices do
         get ':id/customer', to: 'customer#show'
         get ':id/merchant', to: 'merchant#show'
+        get ':id/invoice_items', to: 'invoice_items#index'
       end
 
       resources :customers, only: [:index, :show]
