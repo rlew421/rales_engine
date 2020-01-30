d1e6ccf69e8cbdeda987151a8497a3c73188e9e5
Rachel Lew <rlew07@gmail.com>
2020-01-30 16:17:53 -0700

    Add test that it can return the invoice items associated with one invoice

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -61,4 +61,33 @@ describe "invoices API" do
 
     expect(merchant["data"]["id"]).to eq("#{id}")
   end
+
+  it "sends the invoice items associated with one invoice" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+
+    invoice_1 = customer.invoices.create(merchant: merchant, status: 'shipped')
+    invoice_2 = customer.invoices.create(merchant: merchant, status: 'shipped')
+
+    item_1 = merchant.items.create
+    item_2 = merchant.items.create
+    item_3 = merchant.items.create
+
+    invoice_item_1 = invoice_1.invoice_items.create(item: item_1)
+    invoice_item_2 = invoice_1.invoice_items.create(item: item_2)
+    invoice_item_3 = invoice_1.invoice_items.create(item: item_3)
+    invoice_item_4 = invoice_2.invoice_items.create(item: item_3)
+
+    get "/api/v1/invoices/#{invoice_1.id}/invoice_items"
+
+    expect(response).to be_successful
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(invoice_items["data"].length).to eq(3)
+    expect(invoice_items["data"][0]["attributes"]["id"]).to eq(invoice_item_1.id)
+    expect(invoice_items["data"][1]["attributes"]["id"]).to eq(invoice_item_2.id)
+    expect(invoice_items["data"][2]["attributes"]["id"]).to eq(invoice_item_3.id)
+    expect(invoice_items["data"][3]).to be_nil
+  end
 end
