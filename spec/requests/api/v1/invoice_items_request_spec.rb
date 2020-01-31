ac967546dea9d3ecaf176423d4ee9273ad00d2a3
Rachel Lew <rlew07@gmail.com>
2020-01-30 19:16:11 -0700

    Add test that it can send the invoice associated with an invoice item

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -33,9 +33,29 @@ describe "invoice items API" do
 
     get "/api/v1/invoice_items/#{invoice_item.id}"
 
+    expect(response).to be_successful
+
     invoice_item = JSON.parse(response.body)
 
-    expect(response).to be_successful
     expect(invoice_item["data"]["id"]).to eq("#{id}")
+  end
+
+  it "can send the invoice associated with one invoice item" do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice_1 = customer.invoices.create(merchant: merchant, status: 'shipped')
+    invoice_2 = customer.invoices.create(merchant: merchant, status: 'shipped')
+    item = merchant.items.create
+    invoice_item_1 = invoice_1.invoice_items.create(item: item)
+    invoice_item_2 = invoice_2.invoice_items.create(item: item)
+    id = invoice_1.id
+
+    get "/api/v1/invoice_items/#{invoice_item_1.id}/invoice"
+
+    expect(response).to be_successful
+
+    invoice = JSON.parse(response.body)
+
+    expect(invoice["data"]["id"]).to eq("#{id}")
   end
 end
