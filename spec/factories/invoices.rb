037b67bc00737ec590e5be304836c404648f31e0
Rachel Lew <rlew07@gmail.com>
2020-02-01 13:52:16 -0700

    Add faker time to invoice factory

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :invoice do
     status { 1 }
+    created_at { Faker::Time.backward(days: 30, period: :morning, format: :short) }
+    updated_at { Faker::Time.backward(days: 2, period: :morning, format: :short) }
   end
 end
