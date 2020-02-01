46136d6dd964875a45db626fcc31c23d09ee9f85
Rachel Lew <rlew07@gmail.com>
2020-02-01 13:15:34 -0700

    Add route to find all merchants by query parameters

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         get ':id/items', to: 'items#index'
         get ':id/invoices', to: 'invoices#index'
         get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
         get ':id/favorite_customer', to: 'statistics#favorite_customer'
       end
 
