431ed4a5f551b5b48ec329e987156f9f9a77057c
Rachel Lew <rlew07@gmail.com>
2020-02-02 13:17:28 -0700

    Change storage of item unit price from bigint to float

---
+++ db/migrate/20200202192233_change_unit_price_to_float_in_items.rb
@@ -0,0 +1,5 @@
+class ChangeUnitPriceToFloatInItems < ActiveRecord::Migration[5.2]
+  def change
+    change_column :items, :unit_price, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_184955) do
+ActiveRecord::Schema.define(version: 2020_02_02_192233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2020_01_30_184955) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.bigint "unit_price"
+    t.float "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
