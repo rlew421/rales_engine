9f39d95a56702d5a9d85018d77d6c10013e03ec0
Rachel Lew <rlew07@gmail.com>
2020-02-02 20:30:08 -0700

    Add test for most_revenue model method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -26,5 +26,31 @@ RSpec.describe Merchant, type: :model do
 
       expect(merchant.favorite_customer).to eq(customer_1)
     end
+
+    it "most_revenue" do
+      customer = create(:customer)
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      merchant_3 = create(:merchant)
+
+      item_1 = merchant_1.items.create(name: 'Water Bottle', description: 'Stay hydrated', unit_price: '20.00')
+      item_2 = merchant_2.items.create(name: 'Chocolate bar', description: 'Be careful, it might melt!', unit_price: '2.00')
+      item_3 = merchant_3.items.create(name: 'Sunscreen', description: 'Protect yourself from UV rays', unit_price: '7.00')
+
+      invoice_1 = customer.invoices.create(merchant: merchant_1, status: 'shipped')
+      invoice_item_1 = invoice_1.invoice_items.create(item: item_1, quantity: 5, unit_price: '20.00')
+
+      invoice_2 = customer.invoices.create(merchant: merchant_2, status: 'shipped')
+      invoice_item_2 = invoice_2.invoice_items.create(item: item_2, quantity: 7, unit_price: '2.00')
+
+      invoice_3 = customer.invoices.create(merchant: merchant_3, status: 'shipped')
+      invoice_item_3 = invoice_3.invoice_items.create(item: item_3, quantity: 10, unit_price: '7.00')
+
+      transaction_1 = invoice_1.transactions.create(credit_card_number: '4140149827486249', result: 'success')
+      transaction_2 = invoice_2.transactions.create(credit_card_number: '4140149827486249', result: 'success')
+      transaction_3 = invoice_3.transactions.create(credit_card_number: '4140149827486249', result: 'success')
+
+      expect(Merchant.top_merchants(2)).to eq([merchant_1, merchant_3])
+    end
   end
 end
