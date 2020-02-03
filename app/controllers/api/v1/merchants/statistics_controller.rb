eb3adfd485da5c606dcb18af712bf2b0c0a8ffd4
Rachel Lew <rlew07@gmail.com>
2020-02-02 20:30:45 -0700

    Add top_merchants_by_revenue method

--- app/controllers/api/v1/merchants/statistics_controller.rb
+++ app/controllers/api/v1/merchants/statistics_controller.rb
@@ -5,4 +5,10 @@ class Api::V1::Merchants::StatisticsController < ApplicationController
     serialized = CustomerSerializer.new(customer)
     render json: serialized
   end
+
+  def top_merchants_by_revenue
+    merchants = Merchant.top_merchants(params[:quantity].to_i)
+    serialized = MerchantSerializer.new(merchants)
+    render json: serialized
+  end
 end
